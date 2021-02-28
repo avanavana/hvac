2e1b2b0ac351d3e8c739cddc0312a35d94b5c735
Avana Vana <dear.avana@gmail.com>
2021-02-28 07:18:54 -0500

    Send errors and unwanted stdout to black hole.

--- hvac-cron.sh
+++ hvac-cron.sh
@@ -23,7 +23,7 @@ logs="/private/var/log/hvac"
 current=($(curl -s "https://api.switch-bot.com/v1.0/devices/${SWITCHBOT_METER_ID}/status" --header "Authorization: ${SWITCHBOT_API_KEY}" | /usr/local/bin/jq -r '.body.temperature, .body.humidity'))
 
 # retrieve current heater status
-heater=$(/usr/local/bin/node "${base}/bin/index.js" office-heater)
+heater=$(/usr/local/bin/node "${base}/bin/index.js" office-heater 2>/dev/null)
 
 if (( ${#current[@]} )); then
 
@@ -36,11 +36,11 @@ if (( ${#current[@]} )); then
 
   # turn heater on or off based on temperature preferences and log event or lack thereof
   if [[ ${avg_t} -gt ${max_t} && ${heater} ]] ; then
-    /usr/local/bin/node "${base}/bin/index.js" office-heater off
+    /usr/local/bin/node "${base}/bin/index.js" office-heater off 1>/dev/null 2>&1
     echo " off" >> "${logs}"/temp-humid.log
     # use terminal-notifier (https://github.com/julienXX/terminal-notifier) or IFTTT here to send an "on" notification to your devices
   elif [[ ${avg_t} -lt ${min_t} && ! ${heater} ]] ; then
-    /usr/local/bin/node "${base}/bin/index.js" office-heater on
+    /usr/local/bin/node "${base}/bin/index.js" office-heater on 1>/dev/null 2>&1
     echo " on" >> "${logs}"/temp-humid.log
     # use terminal-notifier (https://github.com/julienXX/terminal-notifier) or IFTTT here to send an "off" notification to your devices
   else
